93030e77e100c6fba810429e9fb9d78ab5ee5fde
EdamAme <121654029+EdamAme-x@users.noreply.github.com>
2023-10-09 12:02:27 +0900

    add

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,5 @@
 import { Component, OnInit } from "@angular/core";
-import {
-  DomSanitizer,
-  SafeHtml,
-  SafeResourceUrl,
-} from "@angular/platform-browser";
+import { DomSanitizer, SafeHtml } from "@angular/platform-browser";
 import { randomInt } from "./utils/randomInt";
 
 @Component({
@@ -13,19 +9,19 @@ import { randomInt } from "./utils/randomInt";
 })
 export class AppComponent implements OnInit {
   title = "rule34";
-  imageUrl: Element = "";
-  id = "";
+  imageUrl: string = "";
+  id: string = "";
   image: SafeHtml = "";
   reloadInterval: number = 5000;
 
-  changeIntervel(event: Event) {
+  changeInterval(event: Event) {
     this.reloadInterval = parseInt((event.target as HTMLInputElement).value);
     localStorage.setItem("reloadInterval", this.reloadInterval.toString());
   }
 
   ngOnInit() {
     this.id = randomInt(1, 8000000).toString();
-    if (typeof window == "undefined") return;
+    if (typeof window === "undefined") return;
 
     if (!localStorage.getItem("reloadInterval")) {
       localStorage.setItem("reloadInterval", "5000");
@@ -34,18 +30,16 @@ export class AppComponent implements OnInit {
     fetch("/image/" + this.id)
       .then((res) => res.text())
       .then((text) => {
-        this.imageUrl = new DOMParser()
-          .parseFromString(text, "text/html")
-          .querySelectorAll("img#image")[0];
+        const parser = new DOMParser();
+        const doc = parser.parseFromString(text, "text/html");
+        const imageUrlElement = doc.querySelector("img#image");
 
-        if (typeof this.imageUrl.getAttribute("src") === null) {
+        if (imageUrlElement === null) {
           window.location.reload();
         }
 
-        this.image = this.sanitizer.bypassSecurityTrustHtml(
-          `<img id="image" src="${this.imageUrl
-            .getAttribute("src")
-            .replace("//samples", "/samples")}">`
+        this.imageUrl = this.sanitizer.bypassSecurityTrustUrl(
+          imageUrlElement.getAttribute("src").replace("//samples", "/samples")
         );
 
         setTimeout(() => {
