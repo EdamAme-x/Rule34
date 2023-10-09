999c45e85d2309cfa719b7e1d8e285eb6c870da3
EdamAme <121654029+EdamAme-x@users.noreply.github.com>
2023-10-09 14:08:33 +0900

    f

--- server.ts
+++ server.ts
@@ -6,9 +6,8 @@ import * as express from "express";
 import { existsSync } from "node:fs";
 import { join } from "node:path";
 import { AppServerModule } from "./src/main.server";
-import axios from "axios";
+import fetch from "node-fetch";
 
-// The Express app is exported so that it can be used by serverless Functions.
 export function app(): express.Express {
   const server = express();
   const distFolder = join(process.cwd(), "dist/rule34/browser");
@@ -16,7 +15,6 @@ export function app(): express.Express {
     ? "index.original.html"
     : "index";
 
-  // Our Universal express-engine (found @ https://github.com/angular/universal/tree/main/modules/express-engine)
   server.engine(
     "html",
     ngExpressEngine({
@@ -27,9 +25,6 @@ export function app(): express.Express {
   server.set("view engine", "html");
   server.set("views", distFolder);
 
-  // Example Express Rest API endpoints
-  // server.get('/api/**', (req, res) => { });
-  // Serve static files from /browser
   server.get(
     "*.*",
     express.static(distFolder, {
@@ -45,14 +40,16 @@ export function app(): express.Express {
   server.get("/image/:id", async (req, res) => {
     const id = req.params.id;
     const url = `https://rule34.xxx/index.php?page=post&s=view&id=${id}`;
-    const resp = await fetch(url);
-    const data = await resp.text();
-
-    res.send(data);
+    try {
+      const resp = await fetch(url);
+      const data = await resp.text();
+      res.send(data);
+    } catch (error) {
+      res.status(500).send(error);
+    }
   });
 
-  // All regular routes use the Universal engine
-  server.get("/", (req, res) => {
+  server.get("/", async (req, res) => {
     res.render(indexHtml, {
       req,
       providers: [{ provide: APP_BASE_HREF, useValue: req.baseUrl }],
@@ -62,11 +59,13 @@ export function app(): express.Express {
   server.get("/get-image/*", async (req, res) => {
     try {
       const url = req.path.replace("/get-image/", "");
-      const response = await axios.get(url, { responseType: "arraybuffer" });
-      const contentType = response.headers["content-type"];
-      res.setHeader("Content-Type", contentType);
-      res.send(response.data);
+      const response = await fetch(url);
+      const contentType = response.headers.get("content-type");
+      res.setHeader("Content-Type", contentType as string);
+      const buffer = await response.buffer();
+      res.send(buffer);
     } catch (error) {
+      console.error(error);
       res.status(500).send(error);
     }
   });
@@ -76,16 +75,12 @@ export function app(): express.Express {
 function run(): void {
   const port = process.env["PORT"] || 4000;
 
-  // Start up the Node server
   const server = app();
   server.listen(port, () => {
     console.log(`Node Express server listening on http://localhost:${port}`);
   });
 }
 
-// Webpack will replace 'require' with '__webpack_require__'
-// '__non_webpack_require__' is a proxy to Node 'require'
-// The below code is to ensure that the server is run only when not requiring the bundle.
 declare const __non_webpack_require__: NodeRequire;
 const mainModule = __non_webpack_require__.main;
 const moduleFilename = (mainModule && mainModule.filename) || "";
